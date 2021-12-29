d49a8889573771ec823eaded628bafc3fadc46a6
Telugu Sravani <telugusravani2@gmail.com>
2021-12-29 18:04:02 +0530

    first commit

--- format.java
+++ format.java
@@ -1,9 +1,9 @@
-public class format {
+public class Format {
     public static void main(String[] args){
         String c = "telugu sravani";
-        int t = 1010;
+        int t = 43;
         float g = 23;
-        System.out.println(String.format("%s", "telugu sravani"));
+        System.out.println(String.format("%s", c));
         System.out.println(String.format("%x", t));
         System.out.println(String.format("%f", g));
     }
