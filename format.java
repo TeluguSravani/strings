9a03d269a9b973dc688bd698a90a00026a6215f6
Telugu Sravani <telugusravani2@gmail.com>
2021-12-29 17:28:28 +0530

    first commit

---
+++ format.java
@@ -0,0 +1,10 @@
+public class format {
+    public static void main(String[] args){
+        String c = "telugu sravani";
+        int t = 1010;
+        float g = 23;
+        System.out.println(String.format("%s", "telugu sravani"));
+        System.out.println(String.format("%x", t));
+        System.out.println(String.format("%f", g));
+    }
+}
