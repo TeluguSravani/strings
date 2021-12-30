7efbbde46eb646afad84517619ac64cef8b2bce8
TeluguSravani <telugusravani2@gmail.com>
2021-12-30 17:11:58 +0530

    first commit

---
+++ Sub.java
@@ -0,0 +1,8 @@
+public class Sub {
+    public static void main(String[] args){
+        String fid = "sreehari";
+        String gid = "sreedevi";
+        System.out.println(fid.substring(1,3));
+        System.out.println(gid.substring(2,7));
+    }
+}
